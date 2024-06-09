2937035a975bea753dec7c535532e1a59a5176ee
DLutsyuk <78265212+DimaL-cloud@users.noreply.github.com>
2024-06-09 19:00:38 +0300

    refactor: remove useless method overriding

--- src/main/java/ua/bibusukraine/fitlifebot/telegram/TelegramBot.java
+++ src/main/java/ua/bibusukraine/fitlifebot/telegram/TelegramBot.java
@@ -63,9 +63,4 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botUsername;
     }
 
-    @Override
-    public void onRegister() {
-        super.onRegister();
-    }
-
 }
